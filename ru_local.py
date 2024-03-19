06764979425706a7416c36bcc66c3287ea6a6b09
ekatellv <159680448+ekatellv@users.noreply.github.com>
2024-03-19 23:26:34 +0700

    Update and rename local.py to ru_local.py

---
+++ ru_local.py
@@ -0,0 +1,8 @@
+# Part of a case-study #4: election
+# Developers: Loseva Ekaterina, Shcherbina Ekaterina, Nesterova Alexandra
+#
+
+who_can_vote = 'Введите количество избирателей в стране: '
+amount_candidates = 'Введите количество кандидатов, участвующих в выборах: '
+amount_pstations = 'Введите количество избирательных участков в стране: '
+results_rules = 'Введите результаты на избирательных участках. Вводите через пробел, первое число – кол-во испорченных бюллетеней, далее данные о кандидатах по списку'
